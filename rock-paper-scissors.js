c4f1ff11ac0f86294b21a275c8068eee76bab6e7
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-06-24 11:24:54 +0900

    Create function playRound()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,4 +22,46 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("Tie!");
+        } else if (computerChoice == "paper") {
+            console.log("You lose! Paper beats rock.");
+            computerScore += 1;
+        } else if (computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors.");
+            humanScore += 1;
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You win! Paper beats rock.");
+            humanScore += 1;
+        } else if (computerChoice == "paper") {
+            console.log("Tie!");
+        } else if (computerChoice == "scissors") {
+            console.log("You lose! Scissors beat paper.");
+            computerScore += 1;
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("You lose! Rock beats scissors.");
+            computerScore += 1;
+        } else if (computerChoice == "paper") {
+            console.log("You win! Scissors beat paper.");
+            humanScore += 1;
+        } else if (computerChoice == "scissors") {
+            console.log("Tie!");
+        }
+    }
+
+    console.log("Your score: " + humanScore);
+    console.log("Computer score: " + computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
