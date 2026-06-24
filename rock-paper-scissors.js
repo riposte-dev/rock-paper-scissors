76ac88746d6113f165d852660e5b0fdce753886a
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-06-24 11:05:07 +0900

    Create function getComputerChoice()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    let computerChoice;
+    let randomNumber = Math.random();
+
+    if (randomNumber <= 0.33) {
+        computerChoice = "rock";
+    } else if (randomNumber <= 0.66) {
+        computerChoice = "paper";
+    } else if (randomNumber <= 1) {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
